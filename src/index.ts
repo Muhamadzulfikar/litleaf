c49708cd483e9a8ed4b46c2dc3856de84e3f80ba
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2024-09-01 23:20:01 +0700

    Add: cors

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import AuthController from "./Controllers/AuthController";
 import NovelController from "./Controllers/NovelController";
 import AuthRequest from "./Requests/AuthRequest";
 import NovelRequest from "./Requests/NovelRequest";
+import { cors } from '@elysiajs/cors'
 
 const app = new Elysia()
+    .use(cors())
     .post("/api/v1/auth/login", AuthController.login, AuthRequest.login)
     .post("/api/v1/auth/register", AuthController.register, AuthRequest.register)
 
     .get("/api/v1/novels", NovelController.getNovel)
     .get("/api/v1/novels/:id", NovelController.getNovelById)
-    .post("/api/v1/novels", NovelController.CreateNovel, NovelRequest.createNovel)
+    .post("/api/v1/novels",  NovelController.CreateNovel, NovelRequest.createNovel)
 
     .onError(({ code }) => {
         if (code === 'NOT_FOUND')
