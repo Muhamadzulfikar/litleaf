7a2fe9e4f6d9ba86d4a061f8c10d179c1dd9baed
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2024-09-15 15:28:41 +0700

    Add: is_private can null and age minimun 10 year old

--- src/Requests/NovelRequest.ts
+++ src/Requests/NovelRequest.ts
@@ -2,15 +2,16 @@ import {t} from "elysia";
 
 const createNovel = {
     body: t.Object({
-        user_uuid: t.String({
-            format: "uuid",
-        }),
         name: t.String(),
         description: t.String(),
         cover: t.String({
             format: "uri"
         }),
         is_publish: t.Boolean(),
+        is_private: t.Optional(t.Boolean()),
+        age: t.Optional(t.Number({
+            minimum: 10,
+        }))
     })
 }
 
